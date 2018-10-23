bd121e1f9454a6ad38d4b7c02340b08b29a72d1a
Alex Lockhart <salexlockhart@gmail.com>
2018-10-23 11:45:53 +0100

    Generate ids on command

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 const alfy = require('alfy');
+const uuid = require('uuid/v1');
+const mongoid = require('mongoid-js');
+
+const ids = {
+	guid: uuid(),
+	mongoid: mongoid()
+};
 
 alfy.output([
 	{
-		title: 'Unicorn',
-		subtitle: alfy.input
+		title: 'GUID',
+		subtitle: ids.guid,
+		arg: ids.guid
+	},
+	{
+		title: 'MongoDB ObjectID',
+		subtitle: ids.mongoid,
+		arg: ids.mongoid
 	}
 ]);
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
+const guidRegex = /(^([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})$)/;
+const mongoRegex = /^[0-9a-fA-F]{24}$/;
+
 test(async t => {
 	const alfy = alfyTest();
-	const result = await alfy('Rainbow');
+	const result = await alfy();
 
-	t.deepEqual(result, [
-		{
-			title: 'Unicorn',
-			subtitle: 'Rainbow'
-		}
-	]);
+	t.deepEqual(result[0].title, 'GUID');
+	t.regex(result[0].subtitle, guidRegex);
+	t.regex(result[0].arg, guidRegex);
+	t.deepEqual(result[0].subtitle, result[0].arg);
+
+	t.deepEqual(result[1].title, 'MongoDB ObjectID');
+	t.regex(result[1].subtitle, mongoRegex);
+	t.regex(result[1].arg, mongoRegex);
+	t.deepEqual(result[1].subtitle, result[1].arg);
 });
